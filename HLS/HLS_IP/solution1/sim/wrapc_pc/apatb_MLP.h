4bebe1fc623325ad0f6cd2369ec6eac67dba18b3
xingyuan123 <xingyuan100@gmail.com>
2024-03-09 15:20:38 +0800

    [Cleanup] Removed useless file (HLS IP)
    Signed-off-by: xingyuan123 <xingyuan100@gmail.com>

--- HLS/HLS_IP/solution1/sim/wrapc_pc/apatb_MLP.h
+++
@@ -1,9 +0,0 @@
-// ==============================================================
-// Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2022.1 (64-bit)
-// Tool Version Limit: 2022.04
-// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
-// ==============================================================
-
-extern "C" void AESL_WRAP_MLP (
-hls::stream<struct ap_axis<32, 0, 0, 0 > > (&in_data),
-hls::stream<struct ap_axis<32, 0, 0, 0 > > (&out_data));
